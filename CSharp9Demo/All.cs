cbccfb3fe8754a9ce5f422ee63c221e47d4cce98
Saheb Irani <sinjul.msbh@yahoo.com>
2020-05-22 12:04:30 +0430

    set codes ..

---
+++ CSharp9Demo/All.cs
@@ -0,0 +1,80 @@
+﻿namespace CSharp9Demo
+{
+    //! Init-only properties
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Person2
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    //! Init accessors and readonly fields
+    public class Person3
+    {
+        private readonly string firstName;
+        private readonly string lastName;
+
+        public string FirstName
+        {
+            get => firstName;
+            init => firstName = (value ?? throw new ArgumentNullException(nameof(FirstName)));
+        }
+        public string LastName
+        {
+            get => lastName;
+            init => lastName = (value ?? throw new ArgumentNullException(nameof(LastName)));
+        }
+    }
+
+    //! Records
+    public data class Person4    
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    //! Data members
+    public data class Person5 { string FirstName; string LastName; }
+
+    public data class Person6
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    //! Positional records
+    public data class Person7
+    {
+        string FirstName;
+        string LastName;
+        public Person7(string firstName, string lastName)
+          => (FirstName, LastName) = (firstName, lastName);
+        public void Deconstruct(out string firstName, out string lastName)
+          => (firstName, lastName) = (FirstName, LastName);
+    }
+
+    public data class Person8(string FirstName, string LastName);
+
+    //! With-expressions and inheritance
+    public data class Person9 { string FirstName; string LastName; }
+    public data class Student : Person { int ID; }
+
+
+    //? Covariant returns
+    abstract class Animal
+    {
+        public abstract Food GetFood();
+    }
+    class Tiger : Animal
+    {
+        public override Meat GetFood() => ...;
+    }
+
+
+
+}
--- CSharp9Demo/Program.cs
+++ CSharp9Demo/Program.cs
@@ -7,6 +7,86 @@ namespace CSharp9Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            new Person2
+            {
+                FirstName = "Scott",
+                LastName = "Hunter"
+            };
+
+
+            //? With-expressions
+            var otherPerson = Person4 with { LastName = "Hanselman" };
+
+            protected Person(Person original) { /* copy all the fields */ } // generated
+
+            //! Value - based equality
+            var originalPerson = otherPerson with { LastName = "Hunter" };
+
+            var person = new Person8("Scott", "Hunter"); // positional construction
+            var (f, l) = person;                        // positional deconstruction
+
+            Person9 person = new Student { FirstName = "Scott", LastName = "Hunter", ID = GetNewId() };
+            otherPerson = person with { LastName = "Hanselman" };
+
+            //? Value-based equality and inheritance
+            Person9 person1 = new Person9 { FirstName = "Scott", LastName = "Hunter" };
+            Person9 person2 = new Student { FirstName = "Scott", LastName = "Hunter", ID = GetNewId() };
+
+
+            //? Improved pattern matching
+            public static decimal CalculateToll(object vehicle) =>
+              vehicle switch
+              {
+
+                  DeliveryTruck t when t.GrossWeightClass > 5000 => 10.00m + 5.00m,
+                  DeliveryTruck t when t.GrossWeightClass < 3000 => 10.00m - 2.00m,
+                  DeliveryTruck _ => 10.00m,
+
+                  _ => throw new ArgumentException("Not a known vehicle type", nameof(vehicle))
+              };
+
+            //? Simple type patterns
+            DeliveryTruck => 10.00m,
+
+            //? Relational patterns
+            DeliveryTruck t when t.GrossWeightClass switch
+            {
+                > 5000 => 10.00m + 5.00m,
+                < 3000 => 10.00m - 2.00m,
+                _ => 10.00m,
+            },
+
+            //? Logical patterns
+            DeliveryTruck t when t.GrossWeightClass switch
+            {
+                < 3000 => 10.00m - 2.00m,
+                >= 3000 and <= 5000 => 10.00m,
+                > 5000 => 10.00m + 5.00m,
+            },
+
+            not null => throw new ArgumentException($"Not a known vehicle type: {vehicle}", nameof(vehicle)),
+            null => throw new ArgumentNullException(nameof(vehicle))
+
+            if (!(e is Customer)) { ... }
+
+            //? You can just say
+            if (e is not Customer) { ... }
+
+            //? Target - typed new expressions
+            Point p = new(3, 5);
+
+            //? Target typed ?? and ?:
+            Person person = student ?? customer; // Shared base type
+            int? result = b ? 0 : null; // nullable value type
+
+
         }
     }
 }
+
+//! Top-level programs
+using System;
+
+Console.WriteLine("Hello World!");
