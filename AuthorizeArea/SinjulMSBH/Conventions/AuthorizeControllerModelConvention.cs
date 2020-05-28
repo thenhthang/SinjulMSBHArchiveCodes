8160da2f2647b9e6276a27d586a24324412cfe5a
Saheb Irani <sinjul.msbh@yahoo.com>
2020-05-28 19:16:50 +0430

    ok baby .. !!!!

--- AuthorizeArea/SinjulMSBH/Conventions/AuthorizeControllerModelConvention.cs
+++ AuthorizeArea/SinjulMSBH/Conventions/AuthorizeControllerModelConvention.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
--- AuthorizeArea/Startup.cs
+++ AuthorizeArea/Startup.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using AuthorizeArea.Data;
+using AuthorizeArea.SinjulMSBH.Conventions;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Mvc.Razor;
-using AuthorizeArea.SinjulMSBH.Conventions;
 
 namespace AuthorizeArea
 {
