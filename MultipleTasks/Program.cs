f1a51d1d72bc00ac52f87ff36fa5e9fef55a8768
Saheb Irani <sinjul.msbh@yahoo.com>
2020-06-11 22:24:04 +0430

    ok baby .. !!!!

---
+++ MultipleTasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultipleTasks
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World .. !!!!");
+
+            Task<int> task1 = Task.Run(() => 13);
+            Task<string> task2 = Task.Run(() => "SinjulMSBH");
+            Task<string> task3 = Task.Run(() => "JackSlater");
+
+            await Task.WhenAll(task1, task2);
+            var task1Result = task1.Result; // or await task1
+            var task2Result = task2.GetAwaiter().GetResult(); // or await task2
+
+            //? This doesn't work
+            //var (task1Result, task2Result) = await Task.WhenAll(task1, task2);
+
+
+            //! This works :)
+            var (t1Result, t2Result, t3Result) =
+                await TaskExtension.WhenAll(task1, task2, task3);
+        }
+    }
+}
---
+++ MultipleTasks/TaskExtension.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+
+namespace MultipleTasks
+{
+    public static class TaskExtension
+    {
+        public static async Task<(T0, T1)> WhenAll<T0, T1>(
+            Task<T0> task0,
+            Task<T1> task1)
+        {
+            await Task.WhenAll(task0, task1).ConfigureAwait(false);
+            // It's ok to use task.Result here as the task is completed
+            return (task0.Result, task1.GetAwaiter().GetResult());
+        }
+
+        public static async Task<(T0, T1, T2)> WhenAll<T0, T1, T2>(
+            Task<T0> task0,
+            Task<T1> task1,
+            Task<T2> task2)
+        {
+            await Task.WhenAll(task0, task1, task2).ConfigureAwait(false);
+            return (task0.Result, task1.Result, task2.Result);
+        }
+
+        public static async Task<(T0, T1, T2, T3)> WhenAll<T0, T1, T2, T3>(
+            Task<T0> task0,
+            Task<T1> task1,
+            Task<T2> task2,
+            Task<T3> task3)
+        {
+            await Task.WhenAll(task0, task1, task2, task3).ConfigureAwait(false);
+            return (task0.Result, task1.Result, task2.Result, task3.Result);
+        }
+
+        public static async Task<(T0, T1, T2, T3, T4)> WhenAll<T0, T1, T2, T3, T4>(
+            Task<T0> task0,
+            Task<T1> task1,
+            Task<T2> task2,
+            Task<T3> task3,
+            Task<T4> task4)
+        {
+            await Task.WhenAll(task0, task1, task2, task3, task4).ConfigureAwait(false);
+
+            return (task0.Result, task1.Result, task2.Result,task3.Result, task4.Result);
+        }
+
+        public static async Task<(T0, T1, T2, T3, T4, T5)> WhenAll<T0, T1, T2, T3, T4, T5>(
+            Task<T0> task0,
+            Task<T1> task1,
+            Task<T2> task2,
+            Task<T3> task3,
+            Task<T4> task4,
+            Task<T5> task5)
+        {
+            await Task.WhenAll(task0, task1, task2, task3, task4, task5).ConfigureAwait(false);
+
+            return (task0.Result, task1.Result, task2.Result,task3.Result, task4.Result, task5.Result);
+        }
+
+        public static async Task<(T0, T1, T2, T3, T4, T5, T6)> WhenAll<T0, T1, T2, T3, T4, T5, T6>(
+            Task<T0> task0,
+            Task<T1> task1,
+            Task<T2> task2,
+            Task<T3> task3,
+            Task<T4> task4,
+            Task<T5> task5,
+            Task<T6> task6)
+        {
+            await Task.WhenAll(task0, task1, task2, task3, task4, task5, task6).ConfigureAwait(false);
+
+            return (task0.Result, task1.Result, task2.Result,task3.Result, task4.Result, task5.Result, task6.Result);
+        }
+    }
+}
