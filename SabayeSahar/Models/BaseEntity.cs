c8ae022ab6bc71f633d480a62fb90835779eb04e
Saheb Irani <sinjul.msbh@yahoo.com>
2020-06-14 18:45:17 +0430

    init ..

---
+++ SabayeSahar/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace SabayeSahar.Models
+{
+    public abstract class BaseEntity 
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SabayeSahar/Models/Order.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SabayeSahar.Models
+{
+    public class Order : BaseEntity
+    {
+        /// <summary>
+        /// زمان ثبت نام
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// هنرجوی ثبت نام شده
+        /// </summary>
+        public virtual Student Student { get; private set; }
+        private int StudentId;    
+
+        /// <summary>
+        /// کد پیگیری دوره ثبت نام شده
+        /// </summary>
+        public string TrackingCode { get; private set; }
+
+        /// <summary>
+        /// مبلغ پرداخت شده
+        /// </summary>
+        public decimal AmountPayed { get; set; }
+
+        /// <summary>
+        /// وضعیت پرداخت
+        /// </summary>
+        public bool IsPayed { get; set; }
+
+        public string Authority { get; set; }
+
+        public Order(Student student, int payTypeId, decimal amountPayed)
+        {
+            DateTime = DateTime.Now;
+            Student = student ?? throw new ArgumentNullException(nameof(student));
+            AmountPayed = amountPayed;
+            TrackingCode = "123456";
+        }
+
+        public Order()
+        {
+        }
+
+    }
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable(nameof(Order), "dbo");
+
+            builder.Metadata.FindNavigation(nameof(Order.Student))
+            .SetPropertyAccessMode(PropertyAccessMode.FieldDuringConstruction);
+            builder.HasOne(o => o.Student)
+                .WithMany()
+                .HasForeignKey(_=>_.Student)
+                .OnDelete(DeleteBehavior.Restrict)
+                .IsRequired();
+
+            builder.Property(q => q.AmountPayed).IsRequired().HasDefaultValue(0);
+        }
+    }
+}
