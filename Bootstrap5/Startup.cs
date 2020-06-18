caeb8be356436adcceae11c97dc090bbeba7fc06
Saheb Irani <sinjul.msbh@yahoo.com>
2020-06-18 16:39:42 +0430

    ok baby ..

---
+++ Bootstrap5/Startup.cs
@@ -0,0 +1,67 @@
+using System.IO;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace Bootstrap5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostEnvironment)
+        {
+            Configuration = configuration;
+            HostEnvironment = hostEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
+            {
+                string libraryPath = Path.GetFullPath(
+                    Path.Combine(HostEnvironment.ContentRootPath, "..", "Bootstrap5"));
+
+                options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            if (HostEnvironment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            //app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ Bootstrap5/wwwroot/js/bs5.js
@@ -0,0 +1,4 @@
+﻿
+import { Tooltip, Toast, Popover } from '../lib/bootstrap-5.0.0-alpha1/dist/js/bootstrap';
+import Alert from '../lib/bootstrap-5.0.0-alpha1/js/dist/alert';
+
