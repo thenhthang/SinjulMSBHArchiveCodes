f8c8289eb71e17174256d1621755e92ebd02ce00
Saheb Irani <sinjul.msbh@yahoo.com>
2020-08-27 10:10:15 +0430

    ok baby .. !!!!

---
+++ VS2019v168Preview2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VS2019v168Preview2
+{
+    class Program
+    {
+        public class SinjulMSBHBase
+        {
+            public int Id { get; set; }
+        }
+
+        public class SinjulMSBH : SinjulMSBHBase
+        {
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            {
+                int max = Math.Max(4, 8);
+
+                _ = typeof(Program).Name;
+                _ = nameof(Program);
+            }
+        }
+    }
+}
