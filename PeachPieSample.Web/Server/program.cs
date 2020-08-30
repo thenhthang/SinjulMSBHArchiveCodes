ec611665c3ed4063eded5eeb4e6789f0884a991f
Saheb Irani <sinjul.msbh@yahoo.com>
2020-08-30 21:18:42 +0430

    ok baby ,, !!!!

---
+++ PeachPieSample.Web/Server/program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PeachPieSample.Web.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
+
+            services.AddPhp(options =>
+            {
+
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.UsePhp();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
