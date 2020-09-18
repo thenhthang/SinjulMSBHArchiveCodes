1b6969f29b14c3a5c1b0c069bcb9bf5f1f72ec56
Saheb Irani <sinjul.msbh@yahoo.com>
2020-09-18 09:57:56 +0430

    OK baby .. !!!!

---
+++ MyThreading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace MyThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerCallback callback = new TimerCallback(PrintTime);
+            Timer timer = new Timer(
+                PrintTime, //callback 
+                "State from Main Method .. !!!!",
+                TimeSpan.FromSeconds(0),
+                TimeSpan.FromSeconds(4));
+
+            Console.WriteLine("Press any key to terminate application .. !!!!");
+            Console.ReadKey();
+        }
+
+        public static void PrintTime(object state) =>
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss") + " {0}", state);
+    }
+}
