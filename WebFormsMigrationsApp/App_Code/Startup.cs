b12db4f8d2cccda748b563a92f577e84a961c7f4
Saheb Irani <sinjul.msbh@yahoo.com>
2020-10-09 20:34:14 +0330

    init .. !!!!

---
+++ WebFormsMigrationsApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsMigrationsApp.Startup))]
+namespace WebFormsMigrationsApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
